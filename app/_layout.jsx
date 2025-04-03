4fbd6e3eeece38848e9ed4c65e2892f623a02fcb
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-04-03 08:27:35 -0300

    feat: add content to layout.jsx

---
+++ app/_layout.jsx
@@ -0,0 +1,21 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: "#3b5998",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="about" options={{ title: "About" }} />
+    </Stack>
+  );
+}
